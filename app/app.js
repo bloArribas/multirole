913314fab965bf38b2e50976f61b1da058389f49
pablo-arribas <pablo.arribas@develapps.com>
2016-05-20 18:15:41 +0200

    First cookie manager implementation trial

--- app/app.js
+++ app/app.js
@@ -1,16 +1,62 @@
 (function () {
+
     'use strict';
 
-// Declare app level module which depends on views, and components
     angular.module('myApp', [
         'ui.router',
         'myApp.view1',
         'myApp.view2',
-        'myApp.version'
-    ]).config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise(function ($injector) {
-            var $state = $injector.get("$state");
-            $state.go('view1');
-        });
-    }]);
+        'myApp.version',
+        'ngCookies'
+    ])
+        .config(['$urlRouterProvider', function ($urlRouterProvider) {
+
+            $urlRouterProvider.otherwise(function ($injector) {
+                var $state = $injector.get("$state");
+                $state.go('view1');
+            });
+
+        }])
+
+        .config(['$urlRouterProvider', '$httpProvider', '$stateProvider', function ($urlRouterProvider, $httpProvider, $stateProvider) {
+
+            $provide.factory('AuthTokenManager', function ($q, $cookies, $location) {
+                return {
+                    request: function (config) {
+
+                        var token = $cookies.get("token");
+                        if (token) {
+
+                            config.headers.Authorization = 'Token ' + token;
+                        }
+
+                        return config;
+                    },
+
+                    response: function (response) {
+
+                        var token = response.data.token;
+                        if (token) {
+
+                            $cookies.put('token', token);
+                        }
+
+                        return response;
+                    },
+
+                    responseError: function (response) {
+                        if (response.status === 401) {
+                            $cookies.remove('token');
+                            $location.path('/');
+                            return;
+                        }
+                        return $q.reject(response);
+                    }
+                };
+            });
+
+            $httpProvider.interceptors.push('AuthTokenManager');
+        }])
+
+
 }());
